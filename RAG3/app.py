9d762e90b666ceac29e7847f4c97499e990ba84e
Aryan Mishra <150948334+aryanmishra11@users.noreply.github.com>
2024-07-06 13:35:18 +0530

    Add files via upload

---
+++ RAG3/app.py
@@ -0,0 +1,137 @@
+import streamlit as st
+import asyncio
+from PyPDF2 import PdfReader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+import os
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+import google.generativeai as genai
+from langchain.vectorstores import FAISS
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
+from langchain_core.messages import AIMessage, HumanMessage
+from langchain_core.output_parsers import StrOutputParser
+
+# Ensure the environment variables are loaded
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Function to handle asyncio event loop
+def get_or_create_eventloop():
+    try:
+        return asyncio.get_event_loop()
+    except RuntimeError as ex:
+        if "There is no current event loop in thread" in str(ex):
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
+            return asyncio.get_event_loop()
+
+get_or_create_eventloop()
+
+def get_pdf_text(pdf_docs):
+    text = ""
+    for pdf in pdf_docs:
+        pdf_reader = PdfReader(pdf)
+        for page in pdf_reader.pages:
+            text += page.extract_text()
+    return text
+
+def get_text_chunks(text):
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=10000, chunk_overlap=1000)
+    chunks = text_splitter.split_text(text)
+    return chunks
+
+def get_vector_store(text_chunks):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    vector_store = FAISS.from_texts(text_chunks, embedding=embeddings)
+    vector_store.save_local("faiss_index")
+
+def get_conversational_chain():
+    prompt_template = """
+    Answer the question as detailed as possible from the provided context, and keep chat history in mind and make sure to provide all the details. Don't give wrong information; give answer from context only. If the question is not related to context, simply respond 'out of context'. Kindly go through the chat history also.
+
+    Context:\n{context}?\n
+    Question:\n{question}\n
+    Answer:
+    """
+    model = ChatGoogleGenerativeAI(model="gemini-pro")
+    prompt = PromptTemplate(template=prompt_template, input_variables=["context", "question"])
+    chain = load_qa_chain(model, chain_type="stuff", prompt=prompt)
+    return chain
+
+def user_input(user_question):
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")
+    
+    new_db = FAISS.load_local("faiss_index", embeddings, allow_dangerous_deserialization=True)
+    docs = new_db.similarity_search(user_question)
+    chain = get_conversational_chain()
+
+    response = chain(
+        {"input_documents": docs, "question": user_question}, StrOutputParser()
+    )
+    
+    return response
+
+def main():
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = [
+            AIMessage(content="Hello! I'm a PDF assistant. Ask me anything about the documents"),
+        ]
+    
+    st.set_page_config(page_title="Chat PDF")
+    st.title("Carnot Research")
+    st.header("Chat with PDF")
+    st.subheader("Upload Your Documents")
+
+    pdf_docs = st.file_uploader("Upload your PDF Files and Click on the Submit & Process Button", accept_multiple_files=True)
+
+    if st.button("Submit & Process"):
+        if pdf_docs:
+            valid_pdfs = True
+            for doc in pdf_docs:
+                if not doc.name.endswith(".pdf"):
+                    valid_pdfs = False
+                    break
+
+            if valid_pdfs:
+                with st.spinner("Processing..."):
+                    raw_text = get_pdf_text(pdf_docs)
+                    text_chunks = get_text_chunks(raw_text)
+                    get_vector_store(text_chunks)
+                    st.success("Done")
+            else:
+                st.error("Please upload files in PDF format.")
+        else:
+            st.error("No files uploaded. Please upload one or more PDF files.")
+
+    for message in st.session_state.chat_history:
+        if isinstance(message, AIMessage):
+            with st.chat_message("AI"):
+                st.markdown(message.content)
+        elif isinstance(message, HumanMessage):
+            with st.chat_message("Human"):
+                st.markdown(message.content)
+
+    user_query = st.chat_input("Type a message...")
+
+    if user_query and user_query.strip() != "":
+        if pdf_docs:
+            st.session_state.chat_history.append(HumanMessage(content=user_query))
+
+            with st.chat_message("Human"):
+                st.markdown(user_query)
+
+            with st.chat_message("AI"):
+                response = user_input(user_query)
+                res = response["output_text"]
+                st.markdown(res)
+                st.session_state.chat_history.append(AIMessage(content=res))
+        else:
+            st.error("No files uploaded. Please upload one or more PDF files.")
+            
+if __name__ == "__main__":
+    try:
+        main()
+    except Exception as e :
+        st.markdown(e)
